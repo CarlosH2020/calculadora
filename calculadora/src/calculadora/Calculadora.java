74b00867ab980040044a2e000502439a7b4b6301
valda <valda@DESKTOP-3NHN8UP>
2020-08-29 17:29:24 -0300

    Teste3

--- calculadora/src/calculadora/Calculadora.java
+++ calculadora/src/calculadora/Calculadora.java
@@ -16,7 +16,7 @@ public class Calculadora {
      */
     public static void main(String[] args) {
         //Carlos
-    	System.out.print("OI");
+    	System.out.print("alo mundo");
     }
     
 }
