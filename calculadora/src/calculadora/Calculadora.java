e60aa01877e4b50ac52ee70493843222ae953f62
DESKTOP <DESKTOP@192.168.2.5>
2020-08-29 17:21:58 -0300

    calc2020

--- calculadora/src/calculadora/Calculadora.java
+++ calculadora/src/calculadora/Calculadora.java
@@ -15,7 +15,8 @@ public class Calculadora {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //testes
+        //Carlos
+    	System.out.print("OI");
     }
     
 }
