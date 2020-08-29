a41baee17a0b69b747387de3bdc1624afc1bd1c5
breno <breno@DESKTOP-VBEOA5Q>
2020-08-29 17:54:56 -0300

    Test2

--- calculadora/src/calculadora/Calculadora.java
+++ calculadora/src/calculadora/Calculadora.java
@@ -16,7 +16,7 @@ public class Calculadora {
      */
     public static void main(String[] args) {
         //Carlos
-    	System.out.print("senha");
+    	System.out.print("345678");
     }
     
 }
